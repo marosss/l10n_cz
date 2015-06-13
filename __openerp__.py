cd72ee1e5aa501ea05777daefd81d2d29dd595b7
Maros Tarbajovsky <MarosTarbajovsky@Maross-MacBook-Air.local>
2015-06-13 19:25:33 +0200

    Initial commit

---
+++ __openerp__.py
@@ -0,0 +1,46 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 EMvolution CZ s.r.o. (<http://www.emvolution.cz>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Accounting for Czech Republic',
+    'version': '2.0',
+    'category': 'Localization/Account Charts',
+    'description': """
+This is the latest modoo localisation necessary to run modoo accounting for CZ with:
+=================================================================================================
+    - CZ chart of accounts
+    - CZ chart of taxes""",
+    'author': 'EMvolution CZ s.r.o.',
+    'website': 'http://www.emvolution.cz',
+    'depends': ['base_iban', 'base_vat', 'account_chart'],
+    'data': [
+        'data/account.account.type.csv',
+        'data/account.account.template.csv',
+        'data/account.tax.code.template.csv',
+        'data/account.chart.template.csv',
+        'data/account.tax.template.csv',
+        'l10n_cz_wizard.xml',
+    ],
+    'demo' : ['demo/demo.xml'],
+    'installable': 'True',
+    'images': ['images/config_chart_l10n_cz.jpeg','images/l10n_cz_chart.jpeg'],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
